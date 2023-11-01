f116de3a8796187d43271abf2f1063e0dae4f1e2
lou320 <lou43404@gmail.com>
2023-11-01 21:57:08 +0630

    new commit

--- monopoly/settings.py
+++ monopoly/settings.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://red-cl16ciis1bgc73f8t150:6379",
+        "LOCATION": "redis://127.0.0.1:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
